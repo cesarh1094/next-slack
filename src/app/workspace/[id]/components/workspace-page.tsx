3e602e86dc7d6cb65b8e260b5f17338a9192dccf
Cesar Hernandez <cesar@mosaicstg.com>
2024-10-11 13:08:45 -0400

    feat: add initial sidebar component functionality

--- src/app/workspace/[id]/components/workspace-page.tsx
+++ src/app/workspace/[id]/components/workspace-page.tsx
@@ -5,5 +5,5 @@ import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
 export function WorkspacePage({ id }: { id: string }) {
   const { data } = useGetWorkspace(id);
 
-  return <div>Data: {JSON.stringify(data, null, "\n")}</div>;
+  return <div>Current workspace page</div>;
 }
